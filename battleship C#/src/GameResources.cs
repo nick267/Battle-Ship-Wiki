7388ae8216f05360ec6ff13ede15cd3e68f3b5a2
VBharwani <54102169+VBharwani@users.noreply.github.com>
2019-09-03 16:34:54 +1000

    Add files via upload
    Additional files

---
+++ battleship C#/src/GameResources.cs
@@ -0,0 +1,211 @@
+﻿using System;
+
+//====================================================================================================
+//The Free Edition of Instant C# limits conversion output to 100 lines per file.
+
+//To subscribe to the Premium Edition, visit our website:
+//https://www.tangiblesoftwaresolutions.com/order/order-instant-csharp.html
+//====================================================================================================
+
+using SwinGameSDK;
+using System.Collections.Generic;
+
+public static class GameResources
+{
+
+	private static void LoadFonts()
+	{
+		NewFont("ArialLarge", "arial.ttf", 80);
+		NewFont("Courier", "cour.ttf", 14);
+		NewFont("CourierSmall", "cour.ttf", 8);
+		NewFont("Menu", "ffaccess.ttf", 8);
+	}
+
+	private static void LoadImages()
+	{
+		//Backgrounds
+		NewImage("Menu", "main_page.jpg");
+		NewImage("Discovery", "discover.jpg");
+		NewImage("Deploy", "deploy.jpg");
+
+		//Deployment
+		NewImage("LeftRightButton", "deploy_dir_button_horiz.png");
+		NewImage("UpDownButton", "deploy_dir_button_vert.png");
+		NewImage("SelectedShip", "deploy_button_hl.png");
+		NewImage("PlayButton", "deploy_play_button.png");
+		NewImage("RandomButton", "deploy_randomize_button.png");
+
+		//Ships
+		int i = 0;
+for (i = 1; i <= 5; i++)
+{
+			NewImage("ShipLR" + i, "ship_deploy_horiz_" + i + ".png");
+			NewImage("ShipUD" + i, "ship_deploy_vert_" + i + ".png");
+		}
+
+		//Explosions
+		NewImage("Explosion", "explosion.png");
+		NewImage("Splash", "splash.png");
+
+	}
+
+	private static void LoadSounds()
+	{
+		NewSound("Error", "error.wav");
+		NewSound("Hit", "hit.wav");
+		NewSound("Sink", "sink.wav");
+		NewSound("Siren", "siren.wav");
+		NewSound("Miss", "watershot.wav");
+		NewSound("Winner", "winner.wav");
+		NewSound("Lose", "lose.wav");
+	}
+
+	private static void LoadMusic()
+	{
+		NewMusic("Background", "horrordrone.mp3");
+	}
+
+	/// <summary>
+	/// Gets a Font Loaded in the Resources
+	/// </summary>
+	/// <param name="font">Name of Font</param>
+	/// <returns>The Font Loaded with this Name</returns>
+
+	public static Font GameFont(string font)
+	{
+		return _Fonts[font];
+	}
+
+	/// <summary>
+	/// Gets an Image loaded in the Resources
+	/// </summary>
+	/// <param name="image">Name of image</param>
+	/// <returns>The image loaded with this name</returns>
+
+	public static Bitmap GameImage(string image)
+	{
+		return _Images[image];
+	}
+
+	/// <summary>
+	/// Gets an sound loaded in the Resources
+	/// </summary>
+	/// <param name="sound">Name of sound</param>
+	/// <returns>The sound with this name</returns>
+
+	public static SoundEffect GameSound(string sound)
+	{
+		return _Sounds[sound];
+	}
+
+	/// <summary>
+	/// Gets the music loaded in the Resources
+	/// </summary>
+	/// <param name="music">Name of music</param>
+	/// <returns>The music with this name</returns>
+
+	public static Music GameMusic(string music)
+	{
+		return _Music[music];
+	}
+
+	private static Dictionary<string, Bitmap> _Images = new Dictionary<string, Bitmap>();
+	private static Dictionary<string, Font> _Fonts = new Dictionary<string, Font>();
+	private static Dictionary<string, SoundEffect> _Sounds = new Dictionary<string, SoundEffect>();
+	private static Dictionary<string, Music> _Music = new Dictionary<string, Music>();
+
+	private static Bitmap _Background;
+	private static Bitmap _Animation;
+	private static Bitmap _LoaderFull;
+	private static Bitmap _LoaderEmpty;
+	private static Font _LoadingFont;
+	private static SoundEffect _StartSound;
+
+	/// <summary>
+	/// The Resources Class stores all of the Games Media Resources, such as Images, Fonts
+	/// Sounds, Music.
+	/// </summary>
+
+	public static void LoadResources()
+	{
+		int width = 0;
+		int height = 0;
+
+		width = SwinGame.ScreenWidth();
+		height = SwinGame.ScreenHeight();
+
+		SwinGame.ChangeScreenSize(800, 600);
+
+		ShowLoadingScreen();
+
+		ShowMessage("Loading fonts...", 0);
+		LoadFonts();
+		SwinGame.Delay(100);
+
+		ShowMessage("Loading images...", 1);
+		LoadImages();
+		SwinGame.Delay(100);
+
+		ShowMessage("Loading sounds...", 2);
+		LoadSounds();
+		SwinGame.Delay(100);
+
+		ShowMessage("Loading music...", 3);
+		LoadMusic();
+		SwinGame.Delay(100);
+
+		SwinGame.Delay(100);
+		ShowMessage("Game loaded...", 5);
+		SwinGame.Delay(100);
+		EndLoadingScreen(width, height);
+	}
+
+	private static void ShowLoadingScreen()
+	{
+		_Background = SwinGame.LoadBitmap(SwinGame.PathToResource("SplashBack.png", ResourceKind.BitmapResource));
+		SwinGame.DrawBitmap(_Background, 0, 0);
+		SwinGame.RefreshScreen();
+		SwinGame.ProcessEvents();
+
+		_Animation = SwinGame.LoadBitmap(SwinGame.PathToResource("SwinGameAni.jpg", ResourceKind.BitmapResource));
+		_LoadingFont = SwinGame.LoadFont(SwinGame.PathToResource("arial.ttf", ResourceKind.FontResource), 12);
+		_StartSound = Audio.LoadSoundEffect(SwinGame.PathToResource("SwinGameStart.ogg", ResourceKind.SoundResource));
+
+		_LoaderFull = SwinGame.LoadBitmap(SwinGame.PathToResource("loader_full.png", ResourceKind.BitmapResource));
+		_LoaderEmpty = SwinGame.LoadBitmap(SwinGame.PathToResource("loader_empty.png", ResourceKind.BitmapResource));
+
+		PlaySwinGameIntro();
+	}
+
+	private static void PlaySwinGameIntro()
+	{
+		const int ANI_CELL_COUNT = 11;
+
+		Audio.PlaySoundEffect(_StartSound);
+		SwinGame.Delay(200);
+
+		int i = 0;
+		for (i = 0; i < ANI_CELL_COUNT; i++)
+		{
+			SwinGame.DrawBitmap(_Background, 0, 0);
+			SwinGame.Delay(20);
+			SwinGame.RefreshScreen();
+			SwinGame.ProcessEvents();
+		}
+
+		SwinGame.Delay(1500);
+
+	}
+
+	private static void ShowMessage(string message, int number)
+	{
+		const int TX = 310;
+		const int TY = 493;
+		const int TW = 200;
+
+//====================================================================================================
+//End of the allowed output for the Free Edition of Instant C#.
+
+//To subscribe to the Premium Edition, visit our website:
+//https://www.tangiblesoftwaresolutions.com/order/order-instant-csharp.html
+//====================================================================================================
