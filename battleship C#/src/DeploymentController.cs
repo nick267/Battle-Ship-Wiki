12abc81c039dac98199f73ff35c57f307048971e
VBharwani <102222589@student.swin.edu.au>
2019-10-09 20:07:20 +1100

    Update DeploymentController.cs

--- battleship C#/src/DeploymentController.cs
+++ battleship C#/src/DeploymentController.cs
@@ -116,7 +116,6 @@ static class DeploymentController
         int row = 0;
         int col = 0;
         row = Convert.ToInt32(Math.Floor((mouse.Y - UtilityFunctions.FIELD_TOP) / (UtilityFunctions.CELL_HEIGHT + UtilityFunctions.CELL_GAP)));
-        
         col = Convert.ToInt32(Math.Floor((mouse.X - UtilityFunctions.FIELD_LEFT) / (UtilityFunctions.CELL_WIDTH + UtilityFunctions.CELL_GAP)));
 
         if (row >= 0 & row < GameController.HumanPlayer.PlayerGrid.Height)
